030bbf0d90aa8ba3d4ca9bfaebbd3ee185701e28
VeraLapidus <lapidus.vera@gmail.com>
2022-10-04 15:15:33 +0300

    changes in models (Car + CarInstance)

--- auto_show/models.py
+++ auto_show/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 from django_countries.fields import CountryField
 
+from car.models import Car
+
 
 class Auto_show(models.Model):
     """ Класс автосалона """
 
-    name = models.CharField(max_length=200, blank=True, verbose_name="Название автосалона")
-    country = CountryField(blank=True, verbose_name="Страна")
+    name = models.CharField(max_length=200, verbose_name="Название автосалона")
+    country = CountryField(verbose_name="Страна")
     year_foundation = models.IntegerField(blank=True, verbose_name='Год основания')
     balance = models.IntegerField(default=0, verbose_name='Баланс автосалона, USD')
-    wish_cars = models.CharField(max_length=500, blank=True, verbose_name="Автомобили к приобретению")
-    list_producers = models.CharField(max_length=500, blank=True, verbose_name="Список поставщиков")
+    wish_cars = models.ManyToManyField(Car, verbose_name="Автомобили к приобретению")
+
+    list_auto = models.CharField(max_length=1500, blank=True, verbose_name="Список автомобилей салона")
+    # можно вытащить по связям Auto_show -mtm- Car -fg- CarInstance #
+
+    list_producers = models.CharField(max_length=1500, blank=True, verbose_name="Список поставщиков")
 
     created = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
--- car/models.py
+++ car/models.py
@@ -1,52 +1,58 @@
 from django.db import models
 
-from auto_show.models import Auto_show
 from producer.models import Producer
 from customer.models import Customer
 
 
 class Car(models.Model):
-    """Класс автомобилей"""
+    """ Класс автомобилей """
 
-    short_name = models.CharField(max_length=200, verbose_name="Сокращенное название автомобиля (н-р, Audi Q5 2015")
-    brand = models.CharField(max_length=200, verbose_name="Бренд автомобиля")
-    model = models.CharField(max_length=200, verbose_name="Модель автомобиля")
-    year_launch = models.IntegerField(blank=True, verbose_name='Год выпуска')
-
-    amount = models.IntegerField(blank=True, verbose_name='Количество')
+    name = models.CharField(max_length=200, verbose_name="Бренд, модель, год автомобиля")
+    color = models.CharField(max_length=200, verbose_name="Цвет автомобиля")
     description = models.TextField(blank=True, verbose_name='Описание')
+
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+    is_active = models.BooleanField(default=True, verbose_name='Активен')
+
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Автомобиль'
+        verbose_name_plural = 'Автомобили'
+
+    def __str__(self):
+        return self.name
+
+
+class CarInstance(models.Model):
+    """ Класс экземпляра автомобиля """
+
+    name = models.ForeignKey(Car, on_delete=models.CASCADE, verbose_name="Автомобиль")
 
     CONDITION = [('wish_auto_show', 'желаемый для автосалона'), ('wish_customer', 'желаемый для покупателя'),
                  ('in_showroom', 'в автосалоне'), ('at_producer', 'у производителя'), ('at_customer', 'у покупателя')]
-    condition = models.CharField(max_length=40, choices=CONDITION, blank=True, null=True, verbose_name='Статус авто')
+    condition = models.CharField(max_length=40, choices=CONDITION, verbose_name='Статус авто')
 
     price = models.IntegerField(blank=True, verbose_name='Цена, USD')
     # discount = models.ForeignKey('Discount', on_delete=models.CASCADE, blank=True, null=True, verbose_name="Акция")
     price_discount = models.IntegerField(blank=True, null=True, verbose_name='Цена со скидкой, USD')
     price_action = models.IntegerField(blank=True, null=True, verbose_name='Цена по акции, USD')
 
-    producers = models.ManyToManyField(Producer, verbose_name='Производитель')
-    auto_shows = models.ManyToManyField(Auto_show, verbose_name='Автосалон')
-    customers = models.ManyToManyField(Customer, verbose_name='Покупатель')
+
+    producers = models.ForeignKey(Producer, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Производитель')
+    # auto_shows = models.ForeignKey(Auto_show, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Автосалон')
+    customers = models.ForeignKey(Customer, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Покупатель')
 
     created = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
     is_active = models.BooleanField(default=True, verbose_name='Активен')
 
-    def short_name(self):
-        """ Сокращенное название автомобиля"""
-
-        short_name = self.brand + self.model + str(self.year_launch)
-        return short_name
-
-
 
     class Meta:
-        # ordering = ['short_name']
-        verbose_name = 'Автомобиль'
-        verbose_name_plural = 'Автомобили'
+        ordering = ['name']
+        verbose_name = 'Экземпляр автомобиля'
+        verbose_name_plural = 'Экземпляры автомобиля'
 
     def __str__(self):
-        return self.short_name
-
-
+        return self.name
